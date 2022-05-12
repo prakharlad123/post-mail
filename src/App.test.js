3397ec3611c67e2b3f52d812f5d746f736135e71
prakhar <prakharlad123@gmail.com>
2022-05-12 14:52:52 +0530

    add test case of login

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,12 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import { Provider } from 'react-redux';
-// import { store } from './app/store';
-// import App from './App';
+import React from 'react';
+import renderer from 'react-test-renderer'
+import Login from '../src/Login';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   );
-
-//   expect(getByText(/learn/i)).toBeInTheDocument();
-// });
+describe('Login', () => {
+    describe('Rendering', () => {
+        it("renders login screen without crash", () => {
+            const login = renderer.create(<Login/>).toJSON();
+            expect(login).toMatchSnapshot();
+        })
+    });
+});
